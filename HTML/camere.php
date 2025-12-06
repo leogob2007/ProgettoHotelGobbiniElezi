00b76a4da3d892151e8fbaf9b077af8256488987
leogob2007 <leonardo.gobbini@polomanettiporciatti.edu.it>
2025-12-06 11:43:15 +0100

    Update camere.php

---
+++ HTML/camere.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<header>
+        <h1>Agriturismo da <span class="pando">Pando</span></h1>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid pel">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active homo" aria-current="page" href="home.html">Home</a>
+                        <a class="nav-link" href="posto.html">Dove siamo?</a>
+                        <a class="nav-link" href="ristorante.php">Il nostro Ristorante</a>
+                        <a class="nav-link" href="camere.php">le nostre camere</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
