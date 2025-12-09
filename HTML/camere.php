73d050cb8331254473c056f37f669d08a1837855
leogob2007 <leonardo.gobbini@polomanettiporciatti.edu.it>
2025-12-09 11:50:13 +0100

    Update camere.php

--- HTML/camere.php
+++ HTML/camere.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../CSS/style.css">
+    <title>Le nostre Camere - Agriturismo da Pando</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/ProgettoHotelGobbiniElezi/CSS/style.css">
 </head>
 <body>
-    
-</body>
-</html>
-
-<header>
+    <header>
         <h1>Agriturismo da <span class="pando">Pando</span></h1>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg">
             <div class="container-fluid pel">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -23,9 +20,21 @@
                         <a class="nav-link active homo" aria-current="page" href="home.html">Home</a>
                         <a class="nav-link" href="posto.html">Dove siamo?</a>
                         <a class="nav-link" href="ristorante.php">Il nostro Ristorante</a>
-                        <a class="nav-link" href="camere.php">le nostre camere</a>
+                        <a class="nav-link" href="camere.php">Le nostre Camere</a>
                     </div>
                 </div>
             </div>
         </nav>
     </header>
+
+    <main class="container mt-5">
+        <h2>Le nostre Camere</h2>
+        <p>Pagina in costruzione. Qui verranno mostrate le camere disponibili e le relative informazioni.</p>
+    </main>
+
+    <footer class="mt-5">
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
